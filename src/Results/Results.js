c49298ee810c23455f0814df21f99be6dda73a53
Cesar Morales <cesarlenin2@live.com>
2020-04-13 15:26:53 -0700

    finished css

--- src/Results/Results.js
+++ src/Results/Results.js
@@ -6,10 +6,14 @@ class Results extends Component {
     render() {
         const bookList = this.props.bookList.map((book,index) => <li key={index}>
             <h2>{book.volumeInfo.title}</h2>
+            <div className="infoContainer">
+            <img src={book.volumeInfo.imageLinks.thumbnail} alt=""/>
+            <div className="info">
             <p>Author:  {book.volumeInfo.authors[0]}</p>
             <p>Price:  ${book.saleInfo.listPrice.amount}</p>
             <p>{book.volumeInfo.description}</p>
-            <img src={book.volumeInfo.imageLinks.thumbnail} alt=""/>
+            </div>
+            </div>
             </li>);
         return(
             <section className="list">
