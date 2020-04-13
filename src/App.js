3b4869201da4b3b99fd1a6adfddf16da40dbd431
Cesar Morales <cesarlenin2@live.com>
2020-04-13 08:33:28 -0700

    added components

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ function App() {
       <header>
         <h1>Google Book Search</h1>
       </header>
+      <main>
+      <Search/>
+      <Filter/>
+      <Results/>
+      </main>
     </div>
   );
 }
