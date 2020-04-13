658c219a8963d275dba44bb55e174dc6a6a10a00
MxJRd <reed182@mail.chapman.edu>
2020-04-13 09:27:58 -0700

    added api fetch

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -5,7 +5,16 @@ export default class Search extends Component{
     // constructor(props){
     //     super(props)
     // }
+    
+    getBook = () => {
+        let url = 'https://www.googleapis.com/books/v1/volumes?q=quilting';
+        let apiKey = "AIzaSyCKMOl3iAm-09f6nFinUXZpel6JDyfyQnc"
+        fetch(url)
+        .then(results => results.json()) //turns results into JSON object
+        .then(resultJSON => console.log(resultJSON))
+    }
     render(){
+        this.getBook();
         return(
             <div>
                 <form type="input">
